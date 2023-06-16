f7aa357bfbafd1223951e5ebb1e6246327edf8ed
kodekloner <billgateokoye@gmail.com>
2023-06-16 13:07:34 -0700

    correction for 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,3 @@
- * main - prints the lowercase alphabet in reverse,
- * followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char ch;
-
-	for (ch = 'z'; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
-}
-
-
-8-print_base16.c
-
 #include <stdio.h>
 
 /**
