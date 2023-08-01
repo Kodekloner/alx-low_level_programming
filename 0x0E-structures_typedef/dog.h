b11481779e98510a2402b2a9f88abe90f24554ca
kodekloner <billgateokoye@gmail.com>
2023-08-01 13:46:05 -0700

    fixed dog.h to betty standard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -17,24 +16,17 @@ struct dog
 	char *owner;
 };
 
-
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
 char *_strcpy(char *dest, char *src);
-
 int _strlen(char *s);
 
-
 #endif
+
